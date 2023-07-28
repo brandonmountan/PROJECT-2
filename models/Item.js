95cf18ee0aca9f1fb52aef5ee2b474fcb5422564
suzyhan <129558186+suzyhan@users.noreply.github.com>
2023-07-27 15:39:49 -1000

    Add item and index.js models

---
+++ models/Item.js
@@ -0,0 +1,54 @@
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection');
+const { Item } = require('.');
+
+class Project extends Model {}
+
+Item.init(
+    {
+        id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            primaryKey: true,
+            autoIncrement: true,
+        },
+        item_name: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+        description: {
+            type: DataTypes.STRING,
+        },
+        price: {
+            type: DataTypes.DECIMAL(10,2),
+            allowNull: false,
+            validate: {
+            isDecimal: true
+            }
+        },
+        start_date: {
+            type: DataTypes.DATE,
+            allowNull: false,
+        },
+        end_date: {
+            type: DataTypes.DATE,
+            allowNull: false,
+        },
+        user_id: {
+            type: DataTypes.INTEGER,
+            references: {
+            model: 'user',
+            key: 'id',
+            },
+        },
+    },
+    {
+        sequelize,
+        timestamps: false,
+        freezeTableName: true,
+        underscored: true,
+        modelName: 'item',
+    }
+);
+
+module.exports = Item;
