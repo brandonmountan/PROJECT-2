6af7fd66a9ee20dbf3fde6c7141880e7ea946923
solowon27 <122332373+solowon27@users.noreply.github.com>
2023-07-31 20:48:58 -0600

    routes gets updated

--- controllers/api/itemRoutes.js
+++ controllers/api/itemRoutes.js
@@ -1,6 +1,9 @@
 const router = require('express').Router();
 const { Item } = require('../../models');
 const withAuth = require('../../utils/auth');
+const upload = require('../../fileUpload');
+const multer = require('multer');
+
 
 router.post('/', withAuth, async (req, res) => {
   try {
@@ -13,6 +16,36 @@ router.post('/', withAuth, async (req, res) => {
   } catch (err) {
     res.status(400).json(err);
   }
+});
+router.post('/upload-item', upload.single('image'), (req, res) => {
+  // Access form data using req.body
+  const item_name = req.body.item_name;
+  const description = req.body.description;
+  const price = req.body.price;
+  const posting_title = req.body.posting_title;
+  const title_max_length = req.body.title_max_length;
+  const category = req.body.category;
+  const zipcode = req.body.zipcode;
+  const current_location = req.body.current_location;
+  const rental_price_period = req.body.rental_price_period;
+  const item_value = req.body.item_value;
+  const quantity_lightbulb = req.body.quantity_lightbulb;
+  const minimum_rental_days = req.body.minimum_rental_days;
+
+  // Access the uploaded file using req.file
+  const image = req.file;
+
+  items.push({
+    item_name,
+    description,
+    price,
+    posting_title,
+    image,
+  });
+
+  res.redirect('/');
+  res.send('Form submitted and file uploaded successfully!');
+
 });
 
 router.get('/search', async (req, res) => {
@@ -29,16 +62,17 @@ router.get('/search', async (req, res) => {
       }
     });
 
-const items = itemData.map((item) => item.get({ plain: true }));
+    const items = itemData.map((item) => item.get({ plain: true }));
+    res.render('search-results', { 
+      items, 
+      logged_in: req.session.logged_in 
+    });
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
 
-res.render('search-results', { 
-  items, 
-  logged_in: req.session.logged_in 
-});
-} catch (err) {
-res.status(500).json(err);
-}
-});
+
 router.delete('/:id', withAuth, async (req, res) => {
   try {
     const itemData = await Item.destroy({
