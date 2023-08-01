657b4ba92d93f5c21b79a959a2aec7053699f57e
Brandon Mountan <brandonmountan1@gmail.com>
2023-07-31 19:42:05 -0600

    day 3, testing...

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -1,29 +1,14 @@
 const router = require('express').Router();
 const { Item, User } = require('../models');
 const withAuth = require('../utils/auth');
-// const imagesData = require('../models/imagesData');
+const imagesData = require('../models/imagesData');
 // const profileData = require('../models/profileData');
 
 router.get('/', async (req, res) => {
   try {
-    // Get all items and JOIN with user data
-    const itemData = await Item.findAll({
-      include: [
-        {
-          model: User,
-        },
-      ],
-    });
-
-    // Serialize data so the template can read it
-    const items = itemData.map((item) => item.get({ plain: true }));
-
-    // Pass serialized data and session flag into template
     res.render('homepage', { 
-      items, 
-      // imagesData,
+      imagesData,
       // profileData,
-      logged_in: req.session.logged_in 
     });
   } catch (err) {
     res.status(500).json(err);
