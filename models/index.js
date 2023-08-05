6a97c5380d9bc7432c9c42d12c6f6418e4900cc7
solowon27 <122332373+solowon27@users.noreply.github.com>
2023-08-05 16:13:31 -0600

    associations are set up again due to cart and item models being changed

--- models/index.js
+++ models/index.js
@@ -1,16 +1,39 @@
 // Import models
 const User = require('./User');
 const Item = require('./Item');
+const Cart = require('./Cart');
 
-// User has many Items
 User.hasMany(Item, {
   foreignKey: 'user_id',
-  onDelete: 'CASCADE'
+  onDelete: 'CASCADE',
 });
 
 // Items belongs to User
 Item.belongsTo(User, {
-  foreignKey: 'user_id'
+  foreignKey: 'user_id',
 });
 
-module.exports = { User, Item };
+// User has many Cart items
+User.hasMany(Cart, {
+  foreignKey: 'user_id',
+  onDelete: 'CASCADE',
+});
+
+// Cart items belong to User
+Cart.belongsTo(User, {
+  foreignKey: 'user_id',
+});
+
+// Items can have many Cart items (through Cart)
+Item.belongsToMany(Cart, {
+  through: 'cart',
+  foreignKey: 'item_id',
+});
+
+// Cart items belong to Item (through Cart)
+Cart.belongsToMany(Item, {
+  through: 'cart',
+  foreignKey: 'cart_id',
+});
+
+module.exports = { User, Item, Cart };
